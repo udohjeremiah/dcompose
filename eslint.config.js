c1246bfc7bf87874eda902725becfad8fafa76ea
Udoh Jeremiah <udohjeremiah@icloud.com>
2025-11-29 18:30:26 +0100

    Add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+// @ts-check
+
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js, "simple-import-sort": eslintPluginSimpleImportSort },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          replacements: {
+            env: false,
+          },
+        },
+      ],
+    },
+  },
+  eslintPluginUnicorn.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: [
+      "node_modules/",
+      "dist/",
+      "build/",
+      "*.tsbuildinfo",
+      "*.log",
+      "coverage/",
+      "pnpm-lock.yaml",
+    ],
+  },
+]);
